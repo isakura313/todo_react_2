99d1418dbdf08784969b31f5899ca12cbdde41f0
Paul <isakura313@gmail.com>
2021-04-13 11:19:00 +0300

    asdad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Sli from './slider/sliderTest'
-const data = 'New-Vegas'
+const slogans = ['Отличный денек',
+                'Я так люблю JSX',
+                'React это жизнь']
+let index_r = Math.floor(Math.random() * slogans.length)
 
 function App() {
   return (
     <div className="App">
       <h1> hello </h1>
-      <h3>{data}</h3>
+      <h3>{slogans[index_r]}</h3>
       <Sli />
     </div>
   );
