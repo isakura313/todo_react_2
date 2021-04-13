679dbcec51d5c57f631c374378a4ac2c6e1521f2
Paul <isakura313@gmail.com>
2021-04-13 13:17:53 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
-import Sli from './slider/sliderTest'
+// import Sli from './slider/sliderTest'
 import Greet from './greet/greet'
+import Film from './film/film'
+
+async function data (){
+  let url = 'https://ghibliapi.herokuapp.com/films'
+  let response = await fetch(url);
+  let json = await response.json();
+  // console.table(json[0])
+  return json
+}
+(async ()=>{
+let info = await data()
+console.log(info[0])
+})()
 
 function App() {
   return (
     <div className="App">
       <h1> hello </h1>
+      <Film props={data()[0]} />
       <Greet />
-      <Sli />
+      {/* <Sli /> */}
     </div>
   );
 }
--- src/film/film.js
+++ src/film/film.js
@@ -1,10 +1,11 @@
 function Film(props) {
     return(
     <div>
-        <h1>{props.title}</h1>
+        {props[0]}
+        {/* <h1>{props.title}</h1>
         <h3> {props.director}</h3>
         <p> {props.description}</p>
-        <span>{props.release_date} </span>
+        <span>{props.release_date} </span> */}
     </div>
     )
   }
